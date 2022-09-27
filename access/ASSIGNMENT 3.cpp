b7f44e1bcf8f848736c9b40f1eec6eba5741adcb
kingjoseph1 <103202480+kingjoseph1@users.noreply.github.com>
2022-09-27 12:48:59 +0000

    Add files via upload

---
+++ access/ASSIGNMENT 3.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+	cout << "ACCESES SECURITY" << "\n" "ENTER EMAIL" <<"\n" "USERNAME:";
+	string name = "name";
+	cin  >> name ;
+	
+	
+	cout << "PASSWORD:";
+	string password = "Password";
+	cin  >> password;
+	
+	
+	string namesOfStudent ="Database :30", classOneStudent="Human comuputer:40", classTwoStudent="Discrete Maths:80", classThreeStudent="Career Development:60",
+	classFourStudent="Data structure:70" "\n" "COMMENTS:";
+	cout  << namesOfStudent <<"\n";
+	cout  <<  classOneStudent <<"\n";
+	cout  <<  classTwoStudent <<"\n";
+	cout  <<  classThreeStudent <<"\n";
+	cout  <<  classFourStudent ;
+	
+	char grader= 'A';
+	char letter; 
+	cin >> letter;
+	
+	cout << "grade from lowwer to highest";	
+	return 0;
+}
